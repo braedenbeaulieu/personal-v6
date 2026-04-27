1136374e2a9e24f7c62a04e2465805e7bc1e66dc
Braeden Beaulieu <60141677+braedenbeaulieu97@users.noreply.github.com>
2026-04-26 21:58:10 -0400

    testing cloudflare server-side functions

---
+++ src/functions/api/ping.js
@@ -0,0 +1,8 @@
+/**
+ * Testing CloudFlare server-side functions
+ */
+export async function onRequestGet() {
+  return new Response(JSON.stringify({ pong: true, time: Date.now() }), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
